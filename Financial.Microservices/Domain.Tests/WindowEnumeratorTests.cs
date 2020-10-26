0c1f38192c405d08f830769fe690ef8602cca265
sp9usb <kamil4521@gmail.com>
2020-10-26 22:41:04 +0100

    Added WindowEnumerator with tests to create moved windows of time series.

---
+++ Financial.Microservices/Domain.Tests/WindowEnumeratorTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Domain.Tests
+{
+    public class WindowEnumeratorTests
+    {
+        [Test]
+        public void WhenAllIsProper_CreateInstance()
+        {
+            var windowEnumerator = new WindowEnumerator<int>(new int[] { }, 10);
+
+            Assert.That(windowEnumerator, Is.Not.Null);
+        }
+
+        [Test]
+        public void WhenDataIsNotDetermined_ThrowException()
+        {
+            Assert.Throws<WindowEnumerator<int>.Exceptions.DataIsNotDeterminedException>(
+                () => _ = new WindowEnumerator<int>(null, 10)
+            );
+        }
+
+        [TestCase(0)]
+        [TestCase(int.MinValue)]
+        [TestCase(-1)]
+        public void WhenWindowSizeIsLessThanOne_ThrowException(int invalidWindowSize)
+        {
+            Assert.Throws<WindowEnumerator<int>.Exceptions.WindowSizeMustBeGreaterThanZeroException>(
+                () => _ = new WindowEnumerator<int>(new int[] { }, invalidWindowSize)
+            );
+        }
+
+        [TestCase(1, 101, 10)]
+        [TestCase(1, 5, 10)]
+        [TestCase(-1000, 1000, 1)]
+        [TestCase(-1000, 1000, 10)]
+        public void Enumerable_WhenAllIsProper(int start, int stop, int windowSize)
+        {
+            var listOfNumbers = Enumerable.Range(start, stop);
+
+            var windowEnumerator = new WindowEnumerator<int>(listOfNumbers, windowSize);
+
+            var expectedStart = start;
+            while (windowEnumerator.MoveNext())
+            {
+                var window = windowEnumerator.Current.ToArray();
+                Assert.That(window.Length, Is.EqualTo(windowSize));
+                CollectionAssert.AreEqual(Enumerable.Range(expectedStart++, windowSize), window);
+            }
+        }
+    }
+}
---
+++ Financial.Microservices/Domain/WindowEnumerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain
+{
+    public class WindowEnumerator<T> : IEnumerator
+    {
+        private readonly IEnumerable<T> _data;
+        private readonly int _windowSize;
+        private int _iteration = 0;
+
+        public IEnumerable<T> Current { get; private set; } = null;
+        object IEnumerator.Current => Current;
+
+        public WindowEnumerator(IEnumerable<T> data, int windowSize)
+        {
+            _data = data ?? throw new Exceptions.DataIsNotDeterminedException();
+            _windowSize = windowSize > 0 ? windowSize : throw new Exceptions.WindowSizeMustBeGreaterThanZeroException();
+        }
+
+        public bool MoveNext()
+        {
+            Current = _data.Skip(_iteration++).Take(_windowSize);
+            return Current.Count() == _windowSize;
+        }
+
+        public void Reset()
+        {
+            _iteration = 0;
+        }
+
+        public static class Exceptions
+        {
+            public class DataIsNotDeterminedException : ArgumentNullException { }
+            public class WindowSizeMustBeGreaterThanZeroException : ArgumentException { }
+        }
+    }
+}
